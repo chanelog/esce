ac2f3e4ecc8817fc7bd94d9d3f5d2c3da1b5b584
PEYX <peyxdev@gmail.com>
2026-04-21 14:15:53 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -5,7 +5,7 @@ file_path="/etc/handeling"
 # Cek apakah file ada
 if [ ! -f "$file_path" ]; then
     # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "PXSTORE SERVER CONNECTED\nGREEN" | sudo tee "$file_path" > /dev/null
+    echo -e "PX SERVER CONNECTED\nGREEN" | sudo tee "$file_path" > /dev/null
     echo "File '$file_path' berhasil dibuat."
 else
     # Jika file ada, cek apakah isinya kosong
